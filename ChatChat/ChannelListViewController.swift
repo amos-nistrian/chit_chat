57f3a1b0a3425f32ce7e15832ce045c41435e769
Amos <amosnistrian@gmail.com>
2018-01-29 12:58:46 -0800

    Querying FB for new channels

--- ChatChat/ChannelListViewController.swift
+++ ChatChat/ChannelListViewController.swift
@@ -20,14 +20,40 @@ class ChannelListViewController: UITableViewController {
     var newChannelTextField: UITextField? // 2
     private var channels: [Channel] = [] // 3
     
+    // will be used to store a reference to the list of channels in the database
+    private lazy var channelRef: DatabaseReference = Database.database().reference().child("channels")
+    // holds a handle to the reference so you can remove it later on.
+    private var channelRefHandle: DatabaseHandle?
+    
+    
+    // MARK: Firebase related methods
+    /*  Query the Firebase database and get a list of channels to show in your table view.
+        1. call observe:with: on your channel reference, storing a handle to the reference. This calls the completion block every time a new channel is added to your database.
+        2. The completion receives a FIRDataSnapshot (stored in snapshot), which contains the data and other helpful methods.
+        3. You pull the data out of the snapshot and, if successful, create a Channel model and add it to your channels array.
+    */
+    private func observeChannels() {
+        // Use the observe method to listen for new
+        // channels being written to the Firebase DB
+        channelRefHandle = channelRef.observe(.childAdded, with: { (snapshot) -> Void in // 1
+            let channelData = snapshot.value as! Dictionary<String, AnyObject> // 2
+            let id = snapshot.key
+            if let name = channelData["name"] as! String!, name.count > 0 { // 3
+                self.channels.append(Channel(id: id, name: name))
+                self.tableView.reloadData()
+            } else {
+                print("Error! Could not decode channel data")
+            }
+        })
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        channels.append(Channel(id: "1", name: "Channel1"))
-        channels.append(Channel(id: "2", name: "Channel2"))
-        channels.append(Channel(id: "3", name: "Channel3"))
-        self.tableView.reloadData()
+//        channels.append(Channel(id: "1", name: "Channel1"))
+//        channels.append(Channel(id: "2", name: "Channel2"))
+//        channels.append(Channel(id: "3", name: "Channel3"))
+//        self.tableView.reloadData()
     }
     
     // MARK: UITableViewDataSource
