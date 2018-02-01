9c30e436ed89945657e8472613cc20336c6a8b2d
Amos <amosnistrian@gmail.com>
2018-01-31 17:18:27 -0800

    saving messages to FB

--- ChatChat/ChatViewController.swift
+++ ChatChat/ChatViewController.swift
@@ -5,6 +5,8 @@ import JSQMessagesViewController
 final class ChatViewController: JSQMessagesViewController {
   
   // MARK: Properties
+    private lazy var messageRef: DatabaseReference = self.channelRef!.child("messages")
+    private var newMessageRefHandle: DatabaseHandle?
     var channelRef: DatabaseReference?
     var channel: Channel? {
         didSet {
@@ -26,18 +28,6 @@ final class ChatViewController: JSQMessagesViewController {
     // No avatars
     collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
     collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
-    
-    // messages from someone else
-    addMessage(withId: "foo", name: "Mr.Bolt", text: "I am so fast!")
-    // messages sent from local sender
-    addMessage(withId: senderId, name: "Me", text: "I bet I can run faster than you!")
-    addMessage(withId: senderId, name: "Me", text: "I like to run!")
-    // animates the receiving of a new message on the view
-    finishReceivingMessage()
-  }
-  
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
   }
   
   // MARK: Collection view data source (and related) methods
@@ -99,6 +89,28 @@ final class ChatViewController: JSQMessagesViewController {
     }
     
   // MARK: Firebase related methods
+    
+    /*
+        1. Using childByAutoId(), you create a child reference with a unique key.
+        2. Then you create a dictionary to represent the message.
+        3. Next, you Save the value at the new child location.
+        4. You then play the canonical “message sent” sound.
+        5. Finally, complete the “send” action and reset the input toolbar to empty.
+    */
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        let itemRef = messageRef.childByAutoId() // 1
+        let messageItem = [ // 2
+            "senderId": senderId!,
+            "senderName": senderDisplayName!,
+            "text": text!,
+            ]
+        
+        itemRef.setValue(messageItem) // 3
+        
+        JSQSystemSoundPlayer.jsq_playMessageSentSound() // 4
+        
+        finishSendingMessage() // 5
+    }
   
   
   // MARK: UI and User Interaction
