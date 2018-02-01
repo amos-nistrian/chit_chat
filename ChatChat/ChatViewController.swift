6f5446c78ecc8745bdaa0c4b4cd8c6ac6b489438
Amos <amosnistrian@gmail.com>
2018-01-31 16:51:31 -0800

    set message bubble colors

--- ChatChat/ChatViewController.swift
+++ ChatChat/ChatViewController.swift
@@ -11,6 +11,9 @@ final class ChatViewController: JSQMessagesViewController {
             title = channel?.name
         }
     }
+    var messages = [JSQMessage]()
+    lazy var outgoingBubbleImageView: JSQMessagesBubbleImage = self.setupOutgoingBubble()
+    lazy var incomingBubbleImageView: JSQMessagesBubbleImage = self.setupIncomingBubble()
   
   // MARK: View Lifecycle
   
@@ -26,8 +29,37 @@ final class ChatViewController: JSQMessagesViewController {
   }
   
   // MARK: Collection view data source (and related) methods
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    private func setupOutgoingBubble() -> JSQMessagesBubbleImage {
+        let bubbleImageFactory = JSQMessagesBubbleImageFactory()
+        return bubbleImageFactory!.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+    }
+    
+    private func setupIncomingBubble() -> JSQMessagesBubbleImage {
+        let bubbleImageFactory = JSQMessagesBubbleImageFactory()
+        return bubbleImageFactory!.incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
+    }
   
-  
+    /*
+        1. Retrieve the message.
+        2. If the message was sent by the local user, return the outgoing image view.
+        3. Otherwise, return the incoming image view.
+    */
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item] // 1
+        if message.senderId == senderId { // 2
+            return outgoingBubbleImageView
+        } else { // 3
+            return incomingBubbleImageView
+        }
+    }
   // MARK: Firebase related methods
   
   
